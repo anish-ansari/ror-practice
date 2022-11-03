763efcd8229cbea02cdbaf81b9fc80b718c5484d
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-03 14:11:58 +0545

    Add create action method

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    # this renders the parameters as a hash
+    # render plain: params[:article].inspect
+  end
 end
