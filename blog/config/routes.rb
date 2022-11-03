75eb545a9fb82223161d56f1a22e5129481287a5
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-03 13:59:25 +0545

    Add new resourceful route

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "welcome/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # adding a resource called articles
+  resources :articles
   # Defines the root path route ("/")
   root "welcome#index"
 end
