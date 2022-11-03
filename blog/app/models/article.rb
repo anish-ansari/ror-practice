0fe95b49d10ff6532efdd4835d2ab6f34a0d0fb9
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-03 17:06:16 +0545

    Add text validator

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  # validator for title, if  < 5 then no @article.save
+  validates :title, presence: true, length: { minimum: 5 }
+  # validates :text, presence: true
 end
