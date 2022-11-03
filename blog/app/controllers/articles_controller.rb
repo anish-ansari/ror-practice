1bc2fcd4a8677e36a6789b97bb8daa90ddea8f9a
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-03 14:37:23 +0545

    Add create operation functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -5,5 +5,16 @@ class ArticlesController < ApplicationController
   def create
     # this renders the parameters as a hash
     # render plain: params[:article].inspect
+
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
