c87556c9ec34335f6a5e6010675b1133f889de5d
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-09 17:06:05 +0545

    Add create and delete functionality

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -2,21 +2,25 @@ class CommentsController < ApplicationController
   def index
   end
 
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
   def create
-  end
-
-  def update
+    # finding where to comment by article_id
+    @article = Article.find(params[:article_id])
+    # creating a new comment using the comment model that is associated to the article model
+    @comment = @article.comments.create(comment_params)
+    # redirecting to the show article page
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
   end
 end
