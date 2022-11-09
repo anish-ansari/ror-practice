d56f88f1fcf356a8a4b209cf4f35e3b70e5ecf7c
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-09 15:04:14 +0545

    Add one to many association

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
   # validator for title, if  < 5 then no @article.save
   validates :title, presence: true, length: { minimum: 5 }
   # validates :text, presence: true
+
+  # associating with comment model
+  has_many :comments
 end
--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  # associating with article model
   belongs_to :article
 end
