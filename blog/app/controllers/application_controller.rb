80260af8ea71050e9b5cdd35f9f2c4ad00c7c178
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-10 17:54:31 +0545

    Include session helper

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  # including the helper here so every other controller can access it
+  include SessionsHelper
+
+  private
+
+  # used to store location and redirect to the login form
+  def logged_in_user
+    # unless user is logged in then they will be redirected to log in first
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      # After the user logs in, the user will be redirected back to the URL they tried to visit
+      redirect_to login_url
+    end
+  end
 end
