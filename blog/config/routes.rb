775f225189bfbb6083d133eeec282fef1d59cef5
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-10 14:38:24 +0545

    Add new routes for signup page

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get "welcome/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/signup", to: "users#new"
+  resources :users
   # adding a resource called articles
   resources :articles do
     # adding a sub resource comments, since comment belongs_to articles
