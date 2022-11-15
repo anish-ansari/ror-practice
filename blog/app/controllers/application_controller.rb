733bc28b77466b291e2c6e47888048179b331db5
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-15 11:48:27 +0545

    Add guard clause in action method

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
   # used to store location and redirect to the login form
   def logged_in_user
     # unless user is logged in then they will be redirected to log in first
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      # After the user logs in, the user will be redirected back to the URL they tried to visit
-      redirect_to login_url
-    end
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    # After the user logs in, the user will be redirected back to the URL they tried to visit
+    redirect_to login_url
   end
 end
