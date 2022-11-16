ed4244190adff01856e76583546c5c449723eecf
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-16 13:05:10 +0545

    Remove unnecessary files

--- blog/app/models/article.rb
+++
@@ -1,5 +0,0 @@
-class Article < ApplicationRecord
-  # validator for title, if  < 5 then no @article.save
-  validates :title, presence: true, length: { minimum: 5 }
-  # validates :text, presence: true
-end
