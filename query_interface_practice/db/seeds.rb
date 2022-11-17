978c6678c43fb3f04511c95a291586cb9075bac7
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-17 13:01:15 +0545

    Add fake data generation

--- query_interface_practice/db/seeds.rb
+++ query_interface_practice/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require_relative '../lib/populator_fix'
+
+Country.populate 5 do |u|
+  u.gdp = Faker::Number.number(digits: 10) # gdp in billions
+  u.name = Faker::Address.country
+end
+
+Person.populate 5 do |u|
+  u.birth_date = Faker::Date.birthday(min_age: 18, max_age: 65)
+  u.country_id = Faker::Number.between(from: 1, to: 5)
+  u.first_name = Faker::Name.first_name
+  u.last_name = Faker::Name.last_name
+end
+
+Company.populate 5 do |u|
+  u.country_id = Faker::Number.between(from: 1, to: 5)
+  u.market_value = Faker::Number.number(digits: 8)
+  u.name = Faker::Company.name
+end
