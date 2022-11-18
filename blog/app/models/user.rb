e2dfb5aac6b1252fd3a675f837b8bc58dcc04f06
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-18 14:17:27 +0545

    Add attachment and image validator

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   # associating with comment model
   has_many :comments
 
+  # associating an avatar attachment
+  has_one_attached :avatar
+
   # a callback to convert email to lowecase before it is saved to the database
   before_save :downcase_email
 
@@ -19,10 +22,26 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   # password validator
   validates :password, presence: true, length: { minimum: 6 }
+  # attachment validator
+  validate :acceptable_image
 
   private
 
   def downcase_email
     self.email = email.downcase
+  end
+
+  def acceptable_image
+    # if image is attached only then will we validate
+    return unless avatar.attached?
+
+    # if file size is greater than 1 MB then too big
+    errors.add(:avatar, 'is too big') unless avatar.byte_size <= 1.megabyte
+
+    # checking if file is jpeg/png or not
+    acceptable_types = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg']
+    return if acceptable_types.include?(avatar.blob.content_type)
+
+    errors.add(:avatar, 'must be a JPEG/JPG, PNG or GIF')
   end
 end
